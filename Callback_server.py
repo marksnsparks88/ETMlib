c9c868ba61be9c62a1e1c1bc05480eb4071c4df3
mark <place@holder.com>
2021-12-12 18:44:51 +0000

    just a bit of tidying

--- Callback_server.py
+++ Callback_server.py
@@ -1,24 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import re
 
-class MyServer(BaseHTTPRequestHandler):
+class Window(BaseHTTPRequestHandler):
     def do_GET(self):
         global auth_code
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<html><head><title>Eve Sign in</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Sign in Complete</p>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This window can now be closed.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
         auth_code = self.path
         
 
 class Callback_Server():
     def __init__(self, hostName, serverPort):
-        self.webServer = HTTPServer((hostName, serverPort), MyServer)
-        print("http://"+hostName+":"+str(serverPort))
+        self.webServer = HTTPServer((hostName, serverPort), Window)
+        print("http://{}:{}".format(hostName, str(serverPort)))
         run = True
         while run:
             self.webServer.handle_request()
@@ -27,8 +27,6 @@ class Callback_Server():
                 self.webServer.server_close()
                 self.code = re.findall("(?:=)(.*?)(?:&)", auth_code)[0]
 
-        
-        
 if __name__ == "__main__":
-    res = Callback_Server("localhost", 8080)
-    print(res.code)
+    response = Callback_Server("localhost", 8080)
+    print(response.code)
